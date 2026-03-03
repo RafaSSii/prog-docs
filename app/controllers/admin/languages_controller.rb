709920fdfa925f85bbe74a950037e9ac93e85440
RafaSSii <rafarsk78@gmail.com>
2026-03-03 16:22:58 -0300

    Feat: Ajustes na tela inicial e adição de usuário de admin

--- app/controllers/admin/languages_controller.rb
+++ app/controllers/admin/languages_controller.rb
@@ -1,2 +1,3 @@
 class Admin::LanguagesController < ApplicationController
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'languages/index'
   get 'languages/show'
   get 'home/index'
